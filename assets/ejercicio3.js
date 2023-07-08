0f34938318852eb78ee8b2627dff997b68d9ec8f
Jairo Erazo <132089241+JairoVErazo@users.noreply.github.com>
2023-07-07 20:56:11 -0600

    avance de navbar y un poco de ejercicio3

---
+++ assets/ejercicio3.js
@@ -0,0 +1,15 @@
+let nombre = document.getElementById('nombre');
+let salario = document.getElementById('salario');
+let categoria = document.getElementById('categorias')
+let valuesCategoria = categoria.value;
+
+if(valuesCategoria =>1){
+    mostrarEmpleado();
+}else{
+    alert("Selecciona una categoría")
+}
+
+function mostrarEmpleado(){
+    let aumento = 0;
+    
+}
